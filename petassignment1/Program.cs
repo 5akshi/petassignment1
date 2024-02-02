c3dc1d9ba6e38e16181bc617a7ca7eaaf7a78e9c
desai <desai@DESKTOP-6RGR9EE>
2024-02-01 23:09:19 -0500

    Add project files.

---
+++ petassignment1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace petassignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
